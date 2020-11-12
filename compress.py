b83784a096021c1f32bae8ec449fbe4359f680aa
ericr333 <erasmussen33@gmaill.com>
2020-11-11 19:19:20 -0500

    Create compress.py
    Here's a file you can use to compress all of our picture files. The size changes drastically from around 200 KB to about 20 KB without any noticable change in resolution. It will make our CNN much faster and easier to evalute. You'll have to change your directory and delete/store the old files after but thats it.

---
+++ compress.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 08:40:07 2020
+
+@author: ericrasmussen
+"""
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+from PIL import Image
+import PIL
+import os
+import glob
+
+def compress_images(directory='/Users/ericrasmussen/Desktop/ML and AI/Project/archive-1/resized train 15', quality=25):
+    # 1. If there is a directory then change into it, else perform the next operations inside of the 
+    # current working directory:
+    if directory:
+        os.chdir(directory)
+
+    # 2. Extract all of the .png and .jpeg files:
+    files = os.listdir()
+
+    # 3. Extract all of the images:
+    images = [file for file in files if file.endswith(('jpg', 'png'))]
+
+    # 4. Loop over every image:
+    for image in images:
+        print(image)
+
+        # 5. Open every image:
+        img = Image.open(image)
+
+        # 5. Compress every image and save it with a new name:
+        img.save("Compressed_"+image, optimize=True, quality=quality)
+        
+compress_images()
