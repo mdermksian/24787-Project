3257b3a02749ba7168e78308376774d15c060841
dhruvah <hdhruva@andrew.cmu.edu>
2020-12-14 13:57:56 -0500

    Updated VGG16 for binary confusion matrix

--- Different Nets/VGG19.py
+++ Different Nets/VGG19.py
@@ -21,20 +21,19 @@ from keras.layers.normalization import BatchNormalization
 import keras
 from keras.models import Model
 from collections import Counter
-from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
 import io
-import tempfile
 
 # gpus = tf.config.experimental.list_physical_devices('GPU')
 # tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2500)])
 
-# photoDirectory = "./Gaussian_Filtered_Class01/"
+photoDirectory = "./Gaussian_Filtered_Class/"
 # photoDirectory = "./Equal_Class_Split_Filtered_Color/"
 # photoDirectory = "./Compressed_Square_Structured/"
 # photoDirectory = "./Cropped_Resized_Structured/"
 # photoDirectory = "./Compressed_Square_Augment_Rotate_Structured/"
-photoDirectory = "./Gaussian_Color_Filters_Equal/"
+# photoDirectory = "./Gaussian_Color_Filters_Equal/"
+
 
 img_height = 224
 img_width = 224
@@ -44,15 +43,13 @@ batchSize = 32
 
 train_datagen = ImageDataGenerator(
   rescale = 1./255,
-  featurewise_center = False,
-  featurewise_std_normalization = False,
-  rotation_range=20,
-  width_shift_range = 0.2,
-  height_shift_range = 0.2,
-  brightness_range=[0.8,1.2],
-  zoom_range=0.2,
-  horizontal_flip=True,
-  vertical_flip = True,
+  # featurewise_center = False,
+  # featurewise_std_normalization = False,
+  # rotation_range=10,
+  # width_shift_range = 0.2,
+  # height_shift_range = 0.2,
+  # zoom_range=0.2,
+  # horizontal_flip=True,
   validation_split=0.20
 )
 
@@ -65,18 +62,75 @@ training_data = train_datagen.flow_from_directory(
     subset='training'
 )
 val_data = train_datagen.flow_from_directory(
-    photoDirectory,
+  photoDirectory,
     target_size=(img_height, img_width),
     batch_size=batchSize,
     class_mode='categorical',
     color_mode="rgb",
-    subset='validation',
-    shuffle=False
+    subset='validation'
 )
 
 # print(training_data.list_files("/Users/ericrasmussen/Desktop/ML and AI/Project/archive-1/resized train 15/*.jpg"))
 num_classes = 5
-classes = [0,1,2,3,4]
+
+# VGG 19 Model
+class VGG19(Sequential):
+    def __init__(self, input_shape):
+        super().__init__()
+
+        self.add(Conv2D(64, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu', input_shape= input_shape))
+        self.add(Conv2D(64, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+
+        self.add(Conv2D(128, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(128, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+
+        self.add(Conv2D(256, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(256, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(256, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(256, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+
+        self.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+
+        self.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+        self.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+
+        self.add(Flatten())
+        self.add(Dense(4096, activation= 'relu'))
+        self.add(Dropout(0.5))
+        self.add(Dense(4096, activation= 'relu'))
+        self.add(Dropout(0.5))
+        self.add(Dense(num_classes, activation= 'softmax'))
+
+        
+                    
+                    
+model = VGG19((224, 224, 3))
 
 
 #buffering to avoid bottlenecks
@@ -93,63 +147,22 @@ classes = [0,1,2,3,4]
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
+num_classes = 5
+classes=[0,1,2,3,4]
 
-vgg16_model = keras.applications.vgg16.VGG16(input_shape = (img_height,img_width,3), weights = 'imagenet') #include_top = True
-# model = Sequential()
-# for layer in vgg16_model.layers[:-1]:
-    # model.add(layer)
-vgg16_model.summary()
-x = vgg16_model.get_layer('fc2').output
-# prediction = Dense(1, activation = 'sigmoid', name = 'predictions')(x)
-prediction = Dense(num_classes, activation = 'softmax', name = 'predictions')(x)
-
-model = Model(inputs = vgg16_model.input, outputs = prediction)
-
-for layer in model.layers:
-    layer.trainable = False
-
-for layer in model.layers[-16:]:
-    layer.trainable = True
-
-def add_regularization(model, regularizer=tf.keras.regularizers.l2(0.00001)):
-
-    if not isinstance(regularizer, tf.keras.regularizers.Regularizer):
-      print("Regularizer must be a subclass of tf.keras.regularizers.Regularizer")
-      return model
-
-    for layer in model.layers:
-        for attr in ['kernel_regularizer']:
-            if hasattr(layer, attr):
-              setattr(layer, attr, regularizer)
-
-    # When we change the layers attributes, the change only happens in the model config file
-    model_json = model.to_json()
-
-    # Save the weights before reloading the model.
-    tmp_weights_path = os.path.join(tempfile.gettempdir(), 'tmp_weights.h5')
-    model.save_weights(tmp_weights_path)
-
-    # load the model from the config
-    model = tf.keras.models.model_from_json(model_json)
     
-    # Reload the model weights
-    model.load_weights(tmp_weights_path, by_name=True)
-    return model
-# x = Flatten()(vgg16_model.output)
+# x = Flatten()(VGG19_model.output)
 # x = Dense(num_classes, activation = 'softmax')(x)
 
-# model.add(Dense(num_classes, activation='softmax'))
+# model = Model(inputs = VGG19_model.input, outputs = x)
 
-# model = Model(inputs = vgg16_model.input, outputs = x)
-model = add_regularization(model, regularizer=tf.keras.regularizers.l2(0.001))
 
-# opt = SGD(lr=0.0001,momentum = .9)
-opt =  Adam(lr = 0.00001)
-#opt =  Adam(lr = 0.00005)
+# opt = SGD(lr=0.0001)
+opt =  Adam(lr = 0.0003)
 model.compile(optimizer= opt, loss='categorical_crossentropy', metrics=['accuracy'])
 # model.compile(optimizer= tf.keras.optimizers.Adam(), loss=tf.keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
-# model.summary()
+model.summary()
 
 #Class Weights
 counter = Counter(training_data.classes)
@@ -157,17 +170,16 @@ max_val = float(max(counter.values()))
 class_weights = {class_id: max_val/num_images for class_id, num_images in counter.items()}
 
 
-runName = 'VGG16Net_GaussianColorEqual_adam_0.00001_shuffleFalse_all'
+runName = 'VGG19_Gaussian_Confusion_Matrix_1'
 # runName = 'AlexNet_Square_Resized_CUDA_50_Batch'
 epochLimit = 500
+
+
+file_writer = tf.summary.create_file_writer('./logs/' + runName + '/cm')
+
 def log_confusion_matrix(epoch, logs):
   # Use the model to predict the values from the validation dataset.
-  test_pred = model.predict(val_data)
-  # test_pred = np.argmax(test_pred,axis=1)
-
-  # test_pred[test_pred >= 0.5] = 1
-  # test_pred[test_pred < 0.5] = 0
-
+  test_pred = model.predict_classes(val_data)
 
   con_mat = tf.math.confusion_matrix(labels=val_data.labels, predictions=test_pred).numpy()
   con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
@@ -195,19 +207,16 @@ def log_confusion_matrix(epoch, logs):
   with file_writer.as_default():
     tf.summary.image("Confusion Matrix", image, step=epoch)
 
-# reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1,
-                              # patience=5, min_lr=0.000000001, cooldown=5,verbose=1)
-
-
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir='./logs/'+runName)
-file_writer = tf.summary.create_file_writer('./logs/' + runName + '/cm')
-cm_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_confusion_matrix)
+
+cm_callback =tf.keras.callbacks.LambdaCallback(on_epoch_end=log_confusion_matrix)
+
 # run tensorboard --logdir 'directory'
 # model.fit_generator(training_data, steps_per_epoch = 1744, validation_data = val_data,validation_steps = 261, epochs = epochLimit, verbose = 2)
 
 # model.fit(training_data,validation_data=val_data,epochs=epochLimit)
 # model.fit(training_data,validation_data=val_data,epochs=epochLimit, callbacks=[tensorboard_callback])
 
-hist = model.fit(training_data, steps_per_epoch = training_data.samples//training_data.batch_size,
-                validation_data = val_data, validation_steps = val_data.samples//val_data.batch_size, 
-                epochs = epochLimit, class_weight = class_weights,callbacks = [tensorboard_callback, cm_callback])
+hist = model.fit(training_data, steps_per_epoch = training_data.samples//training_data.batch_size,validation_data = val_data, 
+validation_steps = val_data.samples//val_data.batch_size,
+epochs = epochLimit,class_weight = class_weights, callbacks = [tensorboard_callback, cm_callback])
